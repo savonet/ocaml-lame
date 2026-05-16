670a953b025045de6ae4137ea30c456c482a5bad
Romain Beauxis <romain.beauxis@gmail.com>
2026-05-16 18:34:13 -0500

    config: use pkg-config when available, fall back to C test

--- src/config/discover.ml
+++ src/config/discover.ml
@@ -13,13 +13,24 @@ int main()
 }
 |}
 
-let link_flags = ["-lmp3lame"; "-lm"]
+let default_flags = ["-lmp3lame"; "-lm"]
 
 let () =
   C.main ~name:"ocaml-lame" (fun c ->
       C.C_define.gen_header_file c ~fname:"config.h"
         [("BIGENDIAN", Switch (is_big_endian ()))];
-
-      assert (C.c_test ~link_flags c has_lame_h_code);
-      C.Flags.write_sexp "c_flags.sexp" [];
-      C.Flags.write_sexp "c_library_flags.sexp" link_flags)
+      let conf =
+        match C.Pkg_config.get c with
+        | Some pc -> (
+            match C.Pkg_config.query_expr_err pc ~package:"mp3lame" ~expr:"mp3lame" with
+            | Ok deps -> deps
+            | Error _ -> (
+                match C.Pkg_config.query_expr_err pc ~package:"lame" ~expr:"lame" with
+                | Ok deps -> deps
+                | Error _ -> { C.Pkg_config.libs = default_flags; cflags = [] }))
+        | None ->
+            assert (C.c_test ~link_flags:default_flags c has_lame_h_code);
+            { C.Pkg_config.libs = default_flags; cflags = [] }
+      in
+      C.Flags.write_sexp "c_flags.sexp" conf.cflags;
+      C.Flags.write_sexp "c_library_flags.sexp" conf.libs)
